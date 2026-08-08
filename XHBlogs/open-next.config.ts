05132a49a1d39795dfc0a31d1583c956f5b8a15f
XingHuiSama <2385744692@qq.com>
2026-08-08 20:42:49 +0800

    chore(cf): 移除 R2，存储统一使用 Appwrite 存储桶

--- XHBlogs/open-next.config.ts
+++ XHBlogs/open-next.config.ts
@@ -1,7 +1,5 @@
 // open-next.config.ts - Cloudflare 适配器配置 (@opennextjs/cloudflare)
+// 不使用 R2 增量缓存，使用默认缓存策略（由 Appwrite 提供内容存储）
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
-});
+export default defineCloudflareConfig({});
